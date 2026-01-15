9b34e9a9acfbda5edcb555a1a83f52be1b780507
Terekhov Artur <terehov.rtr@gmail.com>
2026-01-15 20:00:26 +0300

    Initial commit

---
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -0,0 +1,41 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import javax.annotation.Nonnull;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserstackDriver implements WebDriverProvider {
+    @Nonnull
+    @Override
+    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        MutableCapabilities caps = new MutableCapabilities();
+
+        caps.setCapability("browserstack.user", "terekhovartur_QcANaB");
+        caps.setCapability("browserstack.key", "qLTizRTwPwpF5K3osQBz");
+
+        caps.setCapability("app", "bs://sample.app");
+
+        caps.setCapability("device", "Samsung Galaxy S22");
+        caps.setCapability("os_version", "12.0");
+
+        caps.setCapability("project", "First Java Project");
+        caps.setCapability("build", "browserstack-build-1");
+        caps.setCapability("name", "first_test");
+
+        // Initialise the remote Webdriver using BrowserStack remote URL
+        // and desired capabilities defined above
+        try {
+            return new RemoteWebDriver(
+                    new URL("https://hub.browserstack.com/wd/hub"), caps);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
